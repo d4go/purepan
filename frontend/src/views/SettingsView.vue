cb430bb5b794f703bc6cfa5fafc779dcb359b6a9
d4go <18521338034@163.com>
2026-07-13 21:57:29 +0800

    fix: show macOS window and release v0.1.3

--- frontend/src/views/SettingsView.vue
+++ frontend/src/views/SettingsView.vue
@@ -868,7 +868,7 @@
                     </div>
                     <div class="about-item">
                       <span class="label">版本:</span>
-                      <span class="value">v0.1.2</span>
+                      <span class="value">v0.1.3</span>
                     </div>
                     <div class="about-item">
                       <span class="label">许可证:</span>
